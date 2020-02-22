38114f6c39d216771b34938a2f983eba10036379
Bogumila Dubel <bogumila.dubel@namics.com>
2020-02-22 15:18:05 +0100

    tomcat server - create of backend and frontend module - setup of tomcat server within backend module

---
+++ surveylyzer-backend/src/main/java/servlet/Surveylyzer.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        name = "Surveylyzer",
+        urlPatterns = {"/hello"}
+)
+public class Surveylyzer extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("hello Surveylyzer".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
