98e1f2bb0d593dae907110bac84103d20ab60e8f
travenishere <travenishere@gmail.com>
2020-03-03 14:24:19 +0100

    Testing
    Pom.xml and folder structure expanded for test and added sample test

---
+++ surveylyzer-backend/src/test/java/Sample/SampleTest.java
@@ -0,0 +1,21 @@
+package Sample;
+
+import org.junit.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+
+    @Test
+    // test method to add two values
+    public void testAdd(){
+        int value1 = 3;
+        int value2 = 3;
+        double result = value1 + value2;
+        assertTrue(result == 6);
+    }
+
+
+
+}
