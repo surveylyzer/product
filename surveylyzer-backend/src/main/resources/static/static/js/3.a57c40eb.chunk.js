46af494a658af2058fbb7752dbe0144ed77519a0
Mike <doennmik@students.zhaw.ch>
2020-03-15 12:37:57 +0100

    cleanup: updated gitignore and removed unused files

--- surveylyzer-backend/src/main/resources/static/static/js/3.a57c40eb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpsurveylyzer-frontend"]=this["webpackJsonpsurveylyzer-frontend"]||[]).push([[3],{170:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return r}));var o=e(28),r=(e(7),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,r=document.elementFromPoint(t/2,e/2);if(r){var i=r.closest("ion-content");i&&i.componentOnReady().then((function(){Object(o.l)((function(){return i.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=3.a57c40eb.chunk.js.map
