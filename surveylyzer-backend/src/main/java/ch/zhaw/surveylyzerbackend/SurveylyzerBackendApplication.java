47528eb02c25002b7ac4d5aa65de69958a8f6145
Mike <doennmik@students.zhaw.ch>
2020-03-15 17:34:01 +0100

    added spring component scan for custom package

--- surveylyzer-backend/src/main/java/ch/zhaw/surveylyzerbackend/SurveylyzerBackendApplication.java
+++ surveylyzer-backend/src/main/java/ch/zhaw/surveylyzerbackend/SurveylyzerBackendApplication.java
@@ -2,8 +2,10 @@ package ch.zhaw.surveylyzerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"ch.zhaw"})
 public class SurveylyzerBackendApplication {
 
 	public static void main(String[] args) {
