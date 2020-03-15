55026803e292321260028a2d1aece19c58a4d58b
Mike <doennmik@students.zhaw.ch>
2020-03-15 18:47:51 +0100

    Revert "added spring component scan for custom package"
    This reverts commit 47528eb02c25002b7ac4d5aa65de69958a8f6145.

--- surveylyzer-backend/src/main/java/ch/zhaw/surveylyzerbackend/SurveylyzerBackendApplication.java
+++ surveylyzer-backend/src/main/java/ch/zhaw/surveylyzerbackend/SurveylyzerBackendApplication.java
@@ -2,10 +2,8 @@ package ch.zhaw.surveylyzerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"ch.zhaw"})
 public class SurveylyzerBackendApplication {
 
 	public static void main(String[] args) {
