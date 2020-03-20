1d68761091deb4e292a2336b82a3e252f8d87ca4
Bogumila Dubel <bogumila.dubel@namics.com>
2020-03-20 10:59:29 +0100

    - remove deprecated test case because of fe changes

--- surveylyzer-ui-testing/src/test/java/pages/StartPage.java
+++ surveylyzer-ui-testing/src/test/java/pages/StartPage.java
@@ -9,14 +9,6 @@ import org.openqa.selenium.WebElement;
 @DefaultUrl("page:app.url")
 public class StartPage extends PageObject {
 
-    public void should_see_header_text(String title) {
-        WebElement titleCurrent = find(By.cssSelector("h2"));
-        waitFor(titleCurrent);
-        SoftAssertions soft = new SoftAssertions();
-        soft.assertThat(titleCurrent.getText()).isEqualTo(title);
-        soft.assertAll();
-    }
-
     public void click_result() {
         WebElement viewResults = find(By.cssSelector("[href='/result']"));
         waitFor(viewResults);
--- surveylyzer-ui-testing/src/test/java/stepdefinition/SmokeTestSteps.java
+++ surveylyzer-ui-testing/src/test/java/stepdefinition/SmokeTestSteps.java
@@ -12,11 +12,6 @@ public class SmokeTestSteps {
         startPage.open();
     }
 
-    @Step
-    public void i_should_see_header_text(String title) {
-        startPage.should_see_header_text(title);
-    }
-
     public void i_click_result() {
         startPage.click_result();
     }
