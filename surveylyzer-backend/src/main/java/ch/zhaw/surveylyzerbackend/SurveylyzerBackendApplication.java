53e4c26c6983c90588129e38958069b24a86aa67
Yannic Wetzel <wetzeyan@students.zhaw.ch>
2020-03-21 11:40:49 +0100

    sync pdfPrototype
    sync pdfPrototype

--- surveylyzer-backend/src/main/java/ch/zhaw/surveylyzerbackend/SurveylyzerBackendApplication.java
+++ surveylyzer-backend/src/main/java/ch/zhaw/surveylyzerbackend/SurveylyzerBackendApplication.java
@@ -3,11 +3,18 @@ package ch.zhaw.surveylyzerbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.zhaw.pdffunctionality.PDFAnalyzer;
+
 @SpringBootApplication
 public class SurveylyzerBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SurveylyzerBackendApplication.class, args);
+		
+		//Test the PDF Analyzer
+    	PDFAnalyzer pa = new PDFAnalyzer();
+    	pa.startTest();
+    	// Test end
 	}
 
 }
