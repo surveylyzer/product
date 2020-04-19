c3f9089d78a642040d941fb865b1a98e6d6da0a1
Mike <doennmik@students.zhaw.ch>
2020-04-19 12:23:19 +0200

    update michi's home

--- surveylyzer-frontend/src/pages/Home.tsx
+++ surveylyzer-frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
 import {
-  IonContent, IonPage, IonIcon, IonFab, IonFabButton,
+    IonContent, IonPage, IonIcon, IonFab, IonFabButton,
 } from '@ionic/react';
 import { calculator, construct } from 'ionicons/icons';
 // Custom Components
@@ -10,72 +10,33 @@ import HeaderNav from '../components/HeaderNav';
 // Our CSS
 import './Home.css';
 
-async function CheckStatus() {
-    let templateReceived;
-    let surveyReceived;
-    let passable;
-    await fetch("http://localhost:8080/workflow")
-        .then(response =>{
-            return response.json();
-        })
-        .then((data)=>{
-        templateReceived = data.templateReceived;
-        surveyReceived = data.surveyReceived;
-        alert("Template here? "+templateReceived+" SurveyReceived? "+surveyReceived);
-            if(templateReceived&&surveyReceived){
-                console.log("Template and Survey are here ");
-                passable = true;
-                return passable
-            } else {
-                console.log("Something missing")
-                passable =  false;
-                return passable
-            }
-    })
-
-
-}
-
-
-//   <IonFabButton onClick= {() => {props.history.push('/result')}
-
 const Home: React.FC<RouteComponentProps> = (props) => {
-
-
-    function GoToResult(){
-        //CheckStatus(templateReceived,surveyReceived)
-        let state = CheckStatus();
-        console.log("State: "+ state);
-        if(state){
-            props.history.push('/result')
-        } else {
-            alert("Template or Survey is missing");
-        }
-
+    async function GetStatus(){
+        await fetch("http://localhost:8080/workflow")
+            .then(response => response.json())
+            .then(json => {
+                console.log("ConsoleLog JSON:",json);
+                console.log("ConsoleLog Longing Value:",json.pdfAnalyzerFinished);
+                if(json.pdfAnalyzerFinished === true){
+                    props.history.push('/result');
+                } else {
+                    alert("PDF Analyzer has not yet finished");
+                }
+            });
     }
-
-
     return (
-    <IonPage>
-      <HeaderNav />
-
-      <IonContent>
-
-        <IonFab vertical="bottom" horizontal="end" slot="fixed">
-          <IonFabButton onClick= {()=>GoToResult()
-          }>
-            <IonIcon icon={calculator} />
-          </IonFabButton>
-
-            <IonFabButton onClick={()=> CheckStatus()}>
-                <IonIcon icon={construct} />
-            </IonFabButton>
-        </IonFab>
-
-        <DropArea />
-      </IonContent>
-    </IonPage>
-  );
+        <IonPage>
+            <HeaderNav />
+            <IonContent>
+                <IonFab vertical="bottom" horizontal="end" slot="fixed">
+                    <IonFabButton onClick= {()=>GetStatus()
+                    }>
+                        <IonIcon icon={calculator} />
+                    </IonFabButton>
+                </IonFab>
+                <DropArea />
+            </IonContent>
+        </IonPage>
+    );
 };
-
 export default Home;
