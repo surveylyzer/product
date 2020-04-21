9696601d830d96edce0107deb7c6b6ef0a87d120
Mike <doennmik@students.zhaw.ch>
2020-04-21 23:32:11 +0200

    created db repository

--- surveylyzer-backend/src/main/java/ch/zhaw/domain/SurveyRepository.java
+++ surveylyzer-backend/src/main/java/ch/zhaw/domain/SurveyRepository.java
@@ -3,10 +3,10 @@ package ch.zhaw.domain;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-// We can use: save(), findOne(), findById(), findAll(), count(), delete(), deleteById()
-// ...without implementation!
 @Repository
 public interface SurveyRepository extends MongoRepository<Survey, String> {
+// We can use: save(), findOne(), findById(), findAll(), count(), delete(), deleteById()
+// ...without implementation!
 // More supported query methods:
 // https://docs.spring.io/spring-data/data-document/docs/current/reference/html/#mongodb.repositories.queries
 }
--- surveylyzer-backend/src/main/java/ch/zhaw/domain/SurveyTemplateRepository.java
+++ surveylyzer-backend/src/main/java/ch/zhaw/domain/SurveyTemplateRepository.java
@@ -1,6 +1,12 @@
 package ch.zhaw.domain;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SurveyTemplateRepository extends MongoRepository<SurveyTemplate, String> {
+// We can use: save(), findOne(), findById(), findAll(), count(), delete(), deleteById()
+// ...without implementation!
+// More supported query methods:
+// https://docs.spring.io/spring-data/data-document/docs/current/reference/html/#mongodb.repositories.queries
 }
