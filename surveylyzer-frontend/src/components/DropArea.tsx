722916c7389a8f57775204fd4e8daa6589b9df88
travenishere <travenishere@gmail.com>
2020-04-21 21:17:10 +0200

    Drop Area & Status Update
    fixed duplicate Code & removed Status Update (to be managed by DB)

--- surveylyzer-frontend/src/components/DropArea.tsx
+++ surveylyzer-frontend/src/components/DropArea.tsx
@@ -7,80 +7,29 @@ import { cloudUploadOutline, cloudUpload } from "ionicons/icons";
 const DropArea: React.FC = () => {
     let dragIsActive = false;
 
-
-    //TODO Code duplication of Inputhandling and Status updating
-    function handleTemplateFileInput(fileIn: any[]) {
+    function handleInput(fileIn: any[], inputType:string) {
         dragIsActive = false;
         let file = fileIn[0];
         let arr = file?.name?.split('.');
-        let  pdfType = "templateFile";
         if (arr && arr[arr?.length - 1].toLowerCase() === 'pdf') {
             console.log(file);
         }
         let formData = new FormData();
         formData.append('file1',file);
-        formData.append('pdfType', pdfType);
+        formData.append('pdfType', inputType);
         //Post Template
         fetch('http://localhost:8080/pdf', {
             method: 'POST',
             body: formData
         }).then(response => {
-            console.log("Template File "+file.name+" has been uploaded now");
-            updateStatusTemplateInput(file.name)
+            console.log(inputType+" "+file.name+" has been uploaded now");
         })
     }
-
-    //TODO Code duplication of Inputhandling and Status updating
-    function handleDataFileInput(fileIn: any[]) {
-        dragIsActive = false;
-        let file = fileIn[0];
-        let arr = file?.name?.split('.');
-        let  pdfType = "dataFile";
-        if (arr && arr[arr?.length - 1].toLowerCase() === 'pdf') {
-            console.log(file);
-        }
-        let formData = new FormData();
-        formData.append('file1',file);
-        formData.append('pdfType', pdfType);
-        // Post Data
-        fetch('http://localhost:8080/pdf', {
-            method: 'POST',
-            body: formData
-        }).then(response => {
-            console.log("Data File "+file.name+" has been uploaded");
-            updateStatusDataInput(file.name);
-        })
-    }
-
-
-    //TODO Code duplication of Inputhandling and Status updating
-    function updateStatusTemplateInput(templateName : any){
-        fetch('/workflow',{
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body:  JSON.stringify({"templateReceived":true, "templateName":templateName})
-        }).then(response => {
-            console.log("Status of "+templateName+" has been updated")
-        })
-    }
-
-
-    //TODO Code duplication of Inputhandling and Status updating
-    function updateStatusDataInput(surveyName : any){
-        fetch('/workflow',{
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body:  JSON.stringify({"surveyReceived":true, "surveyName":surveyName})
-        }).then(response => {
-            console.log("Status of "+surveyName+" has been updated")
-        })
-    }
-
 
     return (
         <IonContent>
 
-            <Dropzone onDrop={acceptedFiles => handleTemplateFileInput(acceptedFiles)}
+            <Dropzone onDrop={acceptedFiles => handleInput(acceptedFiles,"templateFile")}
                       onDragEnter={() => dragIsActive = true}
                       onDragLeave={() => dragIsActive = false}>
                 {({ getRootProps, getInputProps }) => (
@@ -97,7 +46,7 @@ const DropArea: React.FC = () => {
                     </section>
                 )}
             </Dropzone>
-            <Dropzone onDrop={acceptedFiles => handleDataFileInput(acceptedFiles)}
+            <Dropzone onDrop={acceptedFiles => handleInput(acceptedFiles,"dataFile")}
                       onDragEnter={() => dragIsActive = true}
                       onDragLeave={() => dragIsActive = false}>
                 {({ getRootProps, getInputProps }) => (
