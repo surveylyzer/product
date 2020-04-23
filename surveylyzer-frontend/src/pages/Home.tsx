c25d128e0deeab324c84b66035de654c799862f8
travenishere <travenishere@gmail.com>
2020-04-23 14:54:20 +0200

    Heroku fix
    Remove unused method

--- surveylyzer-frontend/src/pages/Home.tsx
+++ surveylyzer-frontend/src/pages/Home.tsx
@@ -11,19 +11,7 @@ import HeaderNav from '../components/HeaderNav';
 import './Home.css';
 
 const Home: React.FC<RouteComponentProps> = (props) => {
-    async function GetStatus(){
-        await fetch("http://localhost:8080/workflow")
-            .then(response => response.json())
-            .then(json => {
-                console.log("ConsoleLog JSON:",json);
-                console.log("ConsoleLog Longing Value:",json.pdfAnalyzerFinished);
-                if(json.pdfAnalyzerFinished === true){
-                    props.history.push('/result');
-                } else {
-                    alert("PDF Analyzer has not yet finished");
-                }
-            });
-    }
+
     return (
         <IonPage>
             <HeaderNav />
