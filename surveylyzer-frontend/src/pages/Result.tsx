522cca99bc6430232596138175dee5440a808e6b
Mike <doennmik@students.zhaw.ch>
2020-05-02 17:30:54 +0200

    fix backwards error and removed unused code

--- surveylyzer-frontend/src/pages/Result.tsx
+++ surveylyzer-frontend/src/pages/Result.tsx
@@ -1,5 +1,5 @@
 import { Chart } from "react-google-charts";
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
     IonBackButton,
     IonButtons,
@@ -20,90 +20,100 @@ import { RouteComponentProps } from "react-router";
 import './Result.css';
 
 interface ResultProps {
-    surveyId : string,
-    surveyFile : File
+    surveyId: string,
+    surveyFile: File
 }
 
 const Result: React.FC<RouteComponentProps> = (props) => {
+    // --------------------------------------------
     // Init
-    const myProps : ResultProps = props.location.state as ResultProps || {surveyId:null, surveyFile :null};
+    // --------------------------------------------
     const [resData, setResData] = useState([]);
-    const url = 'http://localhost:8080/resultObject';
-    const urlCsv = 'http://localhost:8080/get-results-csv';
+    const myProps: ResultProps = props.location.state as ResultProps || { surveyId: null, surveyFile: null };
+    const surveyName = myProps?.surveyFile?.name?.replace(".pdf", "");
+    const surveyID = String(myProps?.surveyId || "");
+    const hostURL = window.location.protocol + '//' + window.location.host;
+    const resultURL = hostURL + '/resultObject';
+    const csvURL = hostURL + '/get-results-csv';
+    const rawDataUrl = hostURL + '/rawResults?surveyId=' + surveyID;
 
-    function submitSurveyPdfAndGetResult(file: any, surveyId: string) {
+    // --------------------------------------------
+    // Submit Functions
+    // --------------------------------------------
+    const submitSurveyPdfAndGetResult = useCallback((file: any, surveyId: string) => {
         let formData = new FormData();
         formData.append('file', file);
         formData.append('surveyId', surveyId);
-        fetch(url, {
+        fetch(resultURL, {
             method: 'POST',
             body: formData
         })
             .then(response => response.json())
             .then(json => {
-                if (json.some((row: string | string[]) => row.includes('$$busy$$'))) {
-                    alert('server is still working...');
-                    setTimeout(() => { fetchResult(); }, 2000);
-                }
-                else {
-                    console.log('Fetched json: ', json);
-                    // make all row-arrays the same length (for google charts):
-                    // if json
-                    let maxL = json[0].length;
-                    let res = json.map((row: []) => { return [...row, ...Array(Math.max(maxL-row.length,0)).fill(null)]});
-                    console.log('Googel JSON: ', res);
-                    // update state
-                    setResData(res);
-                }
+                console.log('Fetched json: ', json);
+                // make all row-arrays the same length (for google charts):
+                // if json
+                let maxL = json[0].length;
+                let res = json.map((row: []) => { return [...row, ...Array(Math.max(maxL - row.length, 0)).fill(null)] });
+                console.log('Googel JSON: ', res);
+                // update state
+                setResData(res);
             })
-    }
+    }, []);
 
     function submitSurveyId(surveyId: string) {
         let formData = new FormData();
         formData.append('surveyId', surveyId);
-        fetch(urlCsv, {
+        fetch(csvURL, {
             method: 'POST',
             body: formData
         })
             .then(response => response);
+        // Todo: response ? - error handling...
     }
 
-    const fetchResult = useCallback(() => {
-        fetch(url)
-            .then(response => response.json())
-            .then(json => {
-                if (json.some((row: string | string[]) => row.includes('$$busy$$'))) {
-                    alert('server is still working...');
-                    setTimeout(() => { fetchResult(); }, 2000);
-                }
-                else {
-                    console.log('Fetched json: ', json);
-                    // make all row-arrays the same length (for google charts):
-                    // if json
-                    let maxL = json[0].length;
-                    let res = json.map((row: []) => { return [...row, ...Array(Math.max(maxL-row.length,0)).fill(null)]});
-                    console.log('Googel JSON: ', res);
-                    // update state
-                    setResData(res);
-                }
-            })
-            .catch((err) => { console.log(err); alert('id not found') });
-    }, []);
+    // TODO: Maybe use this for other use case "get result from survey id.."
+    // const fetchResult = useCallback(() => {
+    //     fetch(resultURL)
+    //         .then(response => response.json())
+    //         .then(json => {
+    //             if (json.some((row: string | string[]) => row.includes('$$busy$$'))) {
+    //                 alert('server is still working...');
+    //                 setTimeout(() => { fetchResult(); }, 2000);
+    //             }
+    //             else {
+    //                 console.log('Fetched json: ', json);
+    //                 // make all row-arrays the same length (for google charts):
+    //                 // if json
+    //                 let maxL = json[0].length;
+    //                 let res = json.map((row: []) => { return [...row, ...Array(Math.max(maxL - row.length, 0)).fill(null)] });
+    //                 console.log('Googel JSON: ', res);
+    //                 // update state
+    //                 setResData(res);
+    //             }
+    //         })
+    //         .catch((err) => { console.log(err); alert('id not found') });
+    // }, []);
 
-
+    // --------------------------------------------
     // Fetch Result Data
+    // --------------------------------------------
     // Similar to componentDidMount and componentDidUpdate:
     useEffect(() => {
-        const properties : ResultProps = props.location.state as ResultProps || {surveyId:null, surveyFile :null};
-        submitSurveyPdfAndGetResult(properties.surveyFile, properties.surveyId);
+        // const properties: ResultProps = props.location.state as ResultProps || { surveyId: null, surveyFile: null };
+        if (myProps?.surveyId && myProps?.surveyFile) {
+            submitSurveyPdfAndGetResult(myProps.surveyFile, myProps.surveyId);
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [fetchResult]); // [] --> only on "Mount and Unmount", pass function avoids missing dependency error
+    }, [submitSurveyPdfAndGetResult, myProps]); // [] --> only on "Mount and Unmount", pass function avoids missing dependency error
 
-    const surveyName = myProps?.surveyFile?.name.replace(".pdf", "");
     // eslint-disable-next-line prefer-template
-    const rawDataUrl = 'http://localhost:8080/rawResults?surveyId=' + String(myProps?.surveyId);
 
+    // --------------------------------------------
+    // Render Functions
+    // --------------------------------------------
     function renderData(resData: any) {
+        if (!surveyID) { return <IonCardContent>We're sorry because an Error Happend</IonCardContent>; }
         if (resData.length === 0) {
             return (
                 <div>
@@ -137,8 +147,9 @@ const Result: React.FC<RouteComponentProps> = (props) => {
     }
 
     function renderLinks() {
+        if (!surveyID) { return <IonCardContent>We're sorry because an Error Happend</IonCardContent>; }
         if (resData.length === 0) {
-            return(
+            return (
                 <div>
                     <IonCardSubtitle class={"subtitle"}>Not patient enough to wait for the survey result... ?</IonCardSubtitle>
                     <div className="block">
@@ -146,28 +157,30 @@ const Result: React.FC<RouteComponentProps> = (props) => {
                             <span className="url">{rawDataUrl}</span>
                         </IonCardSubtitle>
                         <IonCardSubtitle>Copy and save this ID to put them in the "Result ID" field on the home page to access the
-                            <b> data visualization: </b> <span className="url">{myProps?.surveyId}</span>
+                            <b> data visualization: </b> <span className="url">{surveyID}</span>
                         </IonCardSubtitle>
                     </div>
                 </div>
             )
         } else {
-            submitSurveyId(myProps.surveyId);
-            return(
+            submitSurveyId(surveyID);
+            return (
                 <div>
                     <IonButton class={"button"} href={"/export-survey-results"}>Export Data as CSV</IonButton>
                     <IonCardSubtitle>Click this link to access the <b>raw data: </b>
                         <a href={rawDataUrl} className="url_ready">{rawDataUrl}</a>
                     </IonCardSubtitle>
                     <IonCardSubtitle>Save this ID to access the
-                        <b> data visualization: </b> <span className="url">{myProps?.surveyId}</span>
+                        <b> data visualization: </b> <span className="url">{surveyID}</span>
                     </IonCardSubtitle>
                 </div>
             )
         }
     }
 
-
+    // --------------------------------------------
+    // Returning UI-Elements
+    // --------------------------------------------
     return (
         <IonPage>
             <IonHeader>
@@ -181,7 +194,7 @@ const Result: React.FC<RouteComponentProps> = (props) => {
             <IonContent>
                 <IonCard class="welcome-card">
                     <IonCardHeader>
-                        <IonCardTitle class={"title"}>{surveyName.toUpperCase()}</IonCardTitle>
+                        <IonCardTitle class={"title"}>{surveyName?.toUpperCase()}</IonCardTitle>
                         {renderLinks()}
                     </IonCardHeader>
                     <IonCardContent>
