d42d058db0d06d2e14e6c5e7befadca623c3cccd
travenishere <travenishere@gmail.com>
2020-05-03 20:02:08 +0200

    Revoke Sonarqube Installation
    Done on CLI but acitve after next commit therefore this dummycommit

--- surveylyzer-backend/src/test/java/ch/zhaw/pdffunctionality/PDFAnalyzerTest_yosepmic.java
+++ surveylyzer-backend/src/test/java/ch/zhaw/pdffunctionality/PDFAnalyzerTest_yosepmic.java
@@ -1,4 +1,26 @@
 package ch.zhaw.pdffunctionality;
 
-public class PDFAnalyzerTest_yosepmic {
+
+import ch.zhaw.surveylyzerbackend.SurveylyzerBackendApplication;
+import net.sourceforge.tess4j.Word;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+
+
+
+    @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SurveylyzerBackendApplication.class)
+    public class PDFAnalyzerTest_yosepmic {
+        PDFAnalyzer pdfAnalyzer = new PDFAnalyzer();
+
+        @Test
+        public void distWordsTest(){
+            Word w1;
+            Word w2;
+            //provoke test commit
+
+
+        }
 }
