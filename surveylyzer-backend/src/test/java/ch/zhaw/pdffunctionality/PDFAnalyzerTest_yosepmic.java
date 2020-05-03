387d5c51759bc865c5e9254c3242e579dabd10cf
travenishere <travenishere@gmail.com>
2020-05-03 21:20:36 +0200

    Test "getWordAngle"
    For Elda to see

--- surveylyzer-backend/src/test/java/ch/zhaw/pdffunctionality/PDFAnalyzerTest_yosepmic.java
+++ surveylyzer-backend/src/test/java/ch/zhaw/pdffunctionality/PDFAnalyzerTest_yosepmic.java
@@ -16,22 +16,42 @@ import java.lang.reflect.Method;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SurveylyzerBackendApplication.class)
-    public class PDFAnalyzerTest_yosepmic {
-        PDFAnalyzer pdfAnalyzer = new PDFAnalyzer();
+public class PDFAnalyzerTest_yosepmic {
+    PDFAnalyzer pdfAnalyzer = new PDFAnalyzer();
 
-        @Test
-        public void distWordsTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-            Rectangle r1 = new Rectangle(0, 0, 2, 4);
-            Rectangle r2 = new Rectangle(5, 0, 2, 4);
-            Word w1 = new Word("WordOne", 0, r1);
-            Word w2 = new Word("WordTwo", 0, r2);
+    @Test
+    public void distWordsTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Rectangle r1 = new Rectangle(0, 0, 2, 4);
+        Rectangle r2 = new Rectangle(5, 0, 2, 4);
+        Word w1 = new Word("WordOne", 0, r1);
+        Word w2 = new Word("WordTwo", 0, r2);
 
-            Method distWords = PDFAnalyzer.class.getDeclaredMethod("distWords",Word.class, Word.class);
-            distWords.setAccessible(true);
-            double distExpected = Math.sqrt(Math.pow(w1.getBoundingBox().getCenterX() - w2.getBoundingBox().getCenterX(), 2)
+        Method distWords = PDFAnalyzer.class.getDeclaredMethod("distWords",Word.class, Word.class);
+        distWords.setAccessible(true);
+        double distExpected = Math.sqrt(Math.pow(w1.getBoundingBox().getCenterX() - w2.getBoundingBox().getCenterX(), 2)
                     + Math.pow(w1.getBoundingBox().getCenterY() - w2.getBoundingBox().getCenterY(), 2));
-            double distActual = (Double) distWords.invoke(pdfAnalyzer,w1,w2);
-            Assert.assertEquals(distExpected, distActual, 0);
+        double distActual = (Double) distWords.invoke(pdfAnalyzer,w1,w2);
+        Assert.assertEquals(distExpected, distActual, 0);
+    }
 
-        }
+    @Test
+    public void getWordAngleTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Rectangle r1 = new Rectangle(0, 0, 2, 4);
+        Rectangle r2 = new Rectangle(5, 0, 2, 4);
+        Word w1 = new Word("WordOne", 0, r1);
+        Word w2 = new Word("WordTwo", 0, r2);
+
+        Method getWordAngle = PDFAnalyzer.class.getDeclaredMethod("getWordAngle", Word.class, Word.class);
+        getWordAngle.setAccessible(true);
+
+        double ak = w1.getBoundingBox().getCenterX() - w2.getBoundingBox().getCenterX();
+        double gk = w1.getBoundingBox().getCenterY() - w2.getBoundingBox().getCenterY();
+
+        double angleExpected = Math.atan(gk)/(ak);
+        double angleActual = (Double) getWordAngle.invoke(pdfAnalyzer,w1,w2);
+
+        Assert.assertEquals(angleExpected, angleActual,0);
+    }
+
+
 }
